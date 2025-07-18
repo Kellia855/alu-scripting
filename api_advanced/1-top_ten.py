82ce284f076300f0897bc1d0b911f936c5d89514
Kellia855 <k.kamikazi@alustudent.com>
2025-07-18 17:45:00 +0000

    Add advanced api tasks

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,31 +1,27 @@
-#!/usr/bin/python3
-
-"""
-function that prints the titles of the first 10 hot posts of a subreddit.
+""""
+Function that prints the titles of the first 10 hot posts of a subreddit.
 """
 
 import requests
 
 
 def top_ten(subreddit):
-    """
-    Prints the titles of the first 10 hot posts for a given subreddit.
-    If the subreddit is invalid, prints None.
-    """
+    """Queries the Reddit API and prints titles of 10 hot posts"""
     url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-    headers = {
-        'User-Agent': 'CustomUserAgent/0.1'
-    }
+    headers = {'User-Agent': 'CustomUserAgent/0.1'}
 
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    
-    if response.status_code == 200:
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+
+        if response.status_code != 200:
+            print("None")
+            return
+
         data = response.json()
-        posts = data.get('data', {}).get('children', [])
+        posts = data.get("data", {}).get("children", [])
+
         for post in posts:
-            print(post['data']['title'])
-    else:
-        print(None)
+            print(post["data"]["title"])
 
     except Exception:
-        print(None)
+        print("None")
