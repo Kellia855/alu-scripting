ff631cebadd045d7c70f2fb22e437e74d8f3dd58
Kellia855 <k.kamikazi@alustudent.com>
2025-07-18 17:04:44 +0000

    Add advanced api tasks

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -12,7 +12,6 @@ def top_ten(subreddit):
     Prints the titles of the first 10 hot posts for a given subreddit.
     If the subreddit is invalid, prints None.
     """
-   
     url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
     headers = {
         'User-Agent': 'python:topten.fetcher:v1.0 (by /u/your_actual_username)'
