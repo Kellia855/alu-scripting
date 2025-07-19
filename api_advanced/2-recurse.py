1efd72c11106f4302c88f3860bbb4b8e8ce174c4
Kellia855 <k.kamikazi@alustudent.com>
2025-07-19 14:09:44 +0000

    Add advanced api tasks

--- api_advanced/2-recurse.py
+++ api_advanced/2-recurse.py
@@ -5,6 +5,10 @@ Recursive function to get all hot article titles of a subreddit.
 import requests
 
 def recurse(subreddit, hot_list=None, after=None):
+"""function that queries the Reddit API and returns a list containing
+the titles of all hot articles for a given subreddit. If no results
+are found for the given subreddit, the function should return None.
+"""
     if hot_list is None:
         hot_list = []
 
